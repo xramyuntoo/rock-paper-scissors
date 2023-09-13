95e3693f7c23e966b29ef8b1ea7d459a3b43ff4b
xramyuntoo <143625055+xramyuntoo@users.noreply.github.com>
2023-09-13 03:26:36 -0400

    Add round Function to script

--- script.js
+++ script.js
@@ -8,16 +8,32 @@ function getComputerChoice() {
     let choiceStr;
     switch(choiceNum) {
         case 0:
-            choiceStr = "Rock";
+            choiceStr = "rock";
             break;
         case 1:
-            choiceStr = "Paper";
+            choiceStr = "paper";
             break;
         case 2:
-            choiceStr = "Scissors";
+            choiceStr = "scissors";
             break;
         default:
             console.log("Error getComputerChoice switch default");
     }
     return choiceStr;
+}
+
+function round(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection == computerSelection) {
+        result = "tie";
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection== "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+            result = "Player Wins!";
+        } else {
+            result = "Computer Wins!";
+        }
+
+        console.log("Player Chose " + playerSelection + ". Computer Chose " + computerSelection +". " + result);
+        return result;
 }
