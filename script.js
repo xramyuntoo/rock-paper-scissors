90e4294e021142d257dd17a035f3d554b4bc6b23
xramyuntoo <143625055+xramyuntoo@users.noreply.github.com>
2023-09-13 02:35:25 -0400

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Yo is this thing on");
+console.log(getComputerChoice());
+
+
+function getComputerChoice() {
+    let choiceNum = Math.floor(Math.random() * 3);
+    console.log("choiceNum = " + choiceNum);
+    let choiceStr;
+    switch(choiceNum) {
+        case 0:
+            choiceStr = "Rock";
+            break;
+        case 1:
+            choiceStr = "Paper";
+            break;
+        case 2:
+            choiceStr = "Scissors";
+            break;
+        default:
+            console.log("Error getComputerChoice switch default");
+    }
+    return choiceStr;
+}
